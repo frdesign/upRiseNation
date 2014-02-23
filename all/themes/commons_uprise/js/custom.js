2553fa9aa62b5167d5c29c8fa08db45c56503c55
frdesign <frdesign212@gmail.com>
2014-02-23 10:28:04 -0500

    Aura value test
    hardcoded value into body, need to test making dynamic

---
+++ all/themes/commons_uprise/js/custom.js
@@ -0,0 +1,10 @@
+        
+        jQuery(document).ready(function($) {
+		// place aura canvas in picture div
+		$( ".block-content>div#aura-container" ).appendTo( $(".pane-user-picture") );
+                // Move links to bio area
+                //$("div.pane-user-field-linkedin-url").appendTo( $(".field-name-field-bio") );
+                //$("div.pane-user-field-facebook-url").appendTo( $(".field-name-field-bio") );
+                //$("div.pane-user-field-twitter-url").appendTo( $(".field-name-field-bio") );
+	});
+        
